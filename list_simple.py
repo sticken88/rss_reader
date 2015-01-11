d356dfcb7db8456e78c584d1cdcca4b9c3dbd81d
sticken88 <matteo.sticco@gmail.com>
2015-01-11 15:36:36 -0800

    added selection and browser opening

--- list_simple.py
+++ list_simple.py
@@ -8,6 +8,7 @@ from kivy.lang import Builder
 from kivy.factory import Factory
 
 import feedparser
+import webbrowser
 
 url_to_read = "http://rss.cnn.com/rss/cnn_topstories.rss"
 
@@ -17,7 +18,7 @@ title_list = {}
 num = len(content["items"])
 i=0
 for item in content["items"]:
-   title_list[str(i)] = {'text': item['title'], 'feed_object': item, 'is_selected': False}
+   title_list[i] = {'title': item['title'], 'link': item['link'], 'is_selected': False}
    i = i+1
 
 # [TODO] Will SelectableView be in the kivy/factory_registers.py,
@@ -30,6 +31,27 @@ Factory.register('ListItemButton', cls=ListItemButton)
 
 Builder.load_file('app_layout.kv')
 
+class FeedsDictAdapter(DictAdapter):
+
+    def rss_feed_changed(self, feeds_dict_adapter, *args):
+       #print "Button pressed" + str(self.counter)
+       feeds = feeds_dict_adapter.data
+
+       webbrowser.open(feeds[0]['link'])
+       print feeds[0]['link']
+#       for feed in feeds:
+#          print feed
+
+       #print feeds_dict_adapter.data['text']
+       #self.counter += 1
+        #if len(fruit_categories_adapter.selection) == 0:
+        #    self.data = {}
+        #    return
+
+        #category = \
+        #        fruit_categories[fruit_categories_adapter.selection[0].text]
+        #self.sorted_keys = category['fruits']
+
 class MainView(GridLayout):
     '''Implementation of a list view with a kv template used for the list
     item class.
@@ -40,31 +62,36 @@ class MainView(GridLayout):
         super(MainView, self).__init__(**kwargs)
 
         list_item_args_converter = \
-                lambda row_index, rec: {'text': rec['text'],
-                                        'is_selected': rec['is_selected'],
+                lambda row_index, feed: {'text': feed['title'],
+                                        'url': feed['link'],
+                                        'is_selected': feed['is_selected'],
                                         'size_hint_y': None,
                                         'height': 30}
 
-        # Here we create a dict adapter with 1..100 integer strings as
-        # sorted_keys, and integers_dict from fixtures as data, passing our
-        # CompositeListItem kv template for the list item view. Then we
-        # create a list view using this adapter. args_converter above converts
-        # dict attributes to ctx attributes.
-        dict_adapter = DictAdapter(sorted_keys=[str(i) for i in range(num)],
+        #feeds_sorted_titles = sorted(title_list.keys())
+
+        feeds_dict_adapter = \
+                         FeedsDictAdapter(
+                                   #sorted_keys=[for i in range(num)],
                                    data=title_list,
                                    args_converter=list_item_args_converter,
+                                   selection_mode='single',
+                                   allow_empty_selection=False,
                                    template='CustomListItem')
 
-        list_view = ListView(adapter=dict_adapter)
+        list_view = ListView(adapter=feeds_dict_adapter)
 
         self.add_widget(list_view)
 
         # So far we created the converter, the adapter to populate the list view and then we added the list to the main view
-    def callback(self, instance):
-       print "Button pressed"
-       
+        #detail_view = FruitDetailView(
+        #        fruit_name=dict_adapter.selection[0].text,
+        #        size_hint=(.7, 1.0))
+
+        feeds_dict_adapter.bind(on_selection_change = feeds_dict_adapter.rss_feed_changed)
 
 
+        #self.add_widget(detail_view)
 
 if __name__ == '__main__':
     from kivy.base import runTouchApp
